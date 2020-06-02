b013edbff7dd6abec09931069cbb52130daf1578
parikshit singh <pk223933@gmail.com>
2020-06-03 05:25:44 +0530

    set up user schema

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+const user_schema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    name:{
+        
+    }
+}, {
+    timestamps:true,
+});
+
+const user=mongoose.model('User', user_schema);
+
+module.exports=user;
