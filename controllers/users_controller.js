ebfe5719e55e420a6c93282eeafdca2934d14765
parikshit singh <pk223933@gmail.com>
2020-06-02 18:30:30 +0530

    installed and used layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,8 @@ module.exports.profile=(req, res)=>
 {
     var options=
     {
-        user_name:"parikshit singh"
+        user_name:"parikshit singh",
+        title:"ComSpace Express",
     }
     return res.render('users_profile', options);
 }
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express=require('express');
 const app=express();
 const port=8000;
 const routes=require('./routes/entry_point_router');
+const expressLayouts=require('express-ejs-layouts');
 
 app.set('case sensitive routing', false);
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
 app.use('/', routes);
 
 app.listen(port, (error)=>
