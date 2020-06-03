a95e933c93b61d9fb8b846fdf35c815a6c64c245
parikshit singh <pk223933@gmail.com>
2020-06-03 12:19:57 +0530

    user sign up implemented

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,38 +1,70 @@
+const User = require('../models/user');
+
 /* USER PROFILE */
-module.exports.profile=(req, res)=>
+module.exports.profile = (req, res) =>
 {
-    var options=
+    var options =
     {
-        user_name:"parikshit singh",
-        title:"ComSpace Express",
+        user_name: "parikshit singh",
+        title: "ComSpace Express",
     }
     return res.render('users_profile', options);
 }
 
 /* USER SIGNUP */
-module.exports.signUp=(req, res)=>
+module.exports.signUp = (req, res) =>
 {
-    var options={
-        title:"ComSpace Express | Sign Up"
+    var options = {
+        title: "ComSpace Express | Sign Up"
     }
     return res.render('user_sign_up', options);
 }
 
 /* USER SIGNIN */
-module.exports.signIn=(req, res)=>
+module.exports.signIn = (req, res) =>
 {
-    var options={
-        title:"ComSpace Express | Sign In"
+    var options = {
+        title: "ComSpace Express | Sign In"
     }
     return res.render('user_sign_in', options);
 }
 
-module.exports.create=(req, res)=>
+/* when we are calling this create function, we are supposing that currently we are on the sign up page. on which we actually are */
+module.exports.create = (req, res) =>
 {
-    //to do later
+    if (req.body.password != req.body.confirm_password)/* if the passwords do not match */
+    {
+        console.log('password does not match from the confirm password field.');
+        return res.redirect('back');
+    }
+    User.findOne({ email: req.body.email }, (error, user) =>
+    {
+        console.log(user);
+        if (error)
+        {
+            console.log('error in finding the user from the database!');
+            return;
+        }
+        if (!user)
+        {
+            User.create(req.body, (error, user) =>
+            {
+                if (error)
+                {
+                    console.log('error in creating user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            });
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    });
 }
 
-module.exports.create_session=(req, res)=>
+module.exports.create_session = (req, res) =>
 {
     //to do later
 }
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const users_controller=require('../controllers/users_controller');
 router.get('/profile', users_controller.profile);
 router.get('/sign-up', users_controller.signUp);
 router.get('/sign-in', users_controller.signIn);
+router.post('/create', users_controller.create);
 
 module.exports=router;
