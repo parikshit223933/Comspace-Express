2234c459fc077b4f417c62e1ff430ea616468b3a
parikshit singh <pk223933@gmail.com>
2020-06-04 23:37:01 +0530

    relocated css files and changed extension to scss

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
 const mongoStore=require('connect-mongo')(session);
+const sassMiddleware=require('node-sass-middleware');
 
+app.use(sassMiddleware(
+    {
+        src:"./assets/scss",
+        dest:'./assets/css',
+        debug:true,
+        outputStyle:"expanded",
+        prefix:'/css',
+    }
+))
 /* for creating and initializing a session */
 /* mongostore is used to store the session cookie in the db */
 app.use(session(
