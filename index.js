90275033b7bd59ee091d9809df5228ef766fe367
parikshit singh <pk223933@gmail.com>
2020-06-04 21:28:17 +0530

    set up mongo store for session cookies

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
+const mongoStore=require('connect-mongo')(session);
 
 /* for creating and initializing a session */
+/* mongostore is used to store the session cookie in the db */
 app.use(session(
     {
         name:'comspace_express',
@@ -21,7 +23,17 @@ app.use(session(
         resave:false,
         cookie:{
             maxAge:(1000*60*100)
-        }
+        },
+        store: new mongoStore(
+            {
+                mongooseConnection:db,
+                autoRemove:'disabled'
+            },
+            function(error)
+            {
+                console.log(err||'connect-mongo setup is working fine');
+            }
+        ),
     }
 ));
 app.use(passport.initialize());
