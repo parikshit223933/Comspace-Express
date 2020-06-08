05324a2f3ed4645bd8bc6710cc808939792f3f8c
parikshit singh <pk223933@gmail.com>
2020-06-09 00:56:19 +0530

    deleting a post (Authorized)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post=require('../models/post');
-
+const Comment=require('../models/comment')
 
 module.exports.create=(req, res)=>
 {
@@ -18,4 +18,35 @@ module.exports.create=(req, res)=>
             return res.redirect('back');
         }
     )
+}
+
+module.exports.destroy=(req, res)=>
+{
+    /* first we need to find whether that post exists in the database or not. */
+    Post.findById(req.params.id, function(error, post)
+    {
+        if(error)
+        {
+            console.log('error in finding the post which needs to be deleted.');
+            return;
+        }
+        // .id means converted the object id to string.
+        if(post.user==req.user.id)
+        {
+            post.remove();
+            Comment.deleteMany({post:req.params.id}, function(err)
+            {
+                if(err)
+                {
+                    console.log('Unable to delete all the comments of the post.');
+                    return;
+                }
+                return res.redirect('back');
+            })
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const passport=require('passport');
 const posts_controller=require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication, posts_controller.create);
+router.get('/destroy/:id', passport.checkAuthentication, posts_controller.destroy);
 
 module.exports=router;
