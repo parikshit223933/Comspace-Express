c656a5c99ac68dd9287a6481117467241c9a6077
parikshit singh <pk223933@gmail.com>
2020-06-09 18:48:07 +0530

    installed connect flash

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/connect-flash/index.d.ts" />
 /// <reference path="globals/express-session/index.d.ts" />
 /// <reference path="globals/mongoose/index.d.ts" />
 /// <reference path="globals/passport/index.d.ts" />
