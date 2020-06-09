463d188c08ede5771d5b71c34f3588401d6c4d07
parikshit singh <pk223933@gmail.com>
2020-06-09 17:52:07 +0530

    converted post controller to async await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,52 +1,55 @@
-const Post=require('../models/post');
-const Comment=require('../models/comment')
+const Post = require('../models/post');
+const Comment = require('../models/comment')
 
-module.exports.create=(req, res)=>
+module.exports.create = async (req, res) =>
 {
-    Post.create(
-        {
-            content:req.body.content,
-            user:req.user._id
-        },
-        function(error, post)
-        {
-            if(error)
+    try
+    {
+        await Post.create(
             {
-                console.log('Error in creating a post!');
-                return;
-            }
-            return res.redirect('back');
-        }
-    )
+                content: req.body.content,
+                user: req.user._id
+            });
+        return res.redirect('back');
+    }
+    catch (error)
+    {
+        console.log('error in creating a post!', error);
+        return;
+    }
+
 }
 
-module.exports.destroy=(req, res)=>
-{
-    /* first we need to find whether that post exists in the database or not. */
-    Post.findById(req.params.id, function(error, post)
+module.exports.destroy = async (req, res) =>
+{   
+    try
     {
-        if(error)
-        {
-            console.log('error in finding the post which needs to be deleted.');
-            return;
-        }
+        /* first we need to find whether that post exists in the database or not. */
+        let post = await Post.findById(req.params.id);
+
         // .id means converted the object id to string.
-        if(post.user==req.user.id)
+        // if the id of the post to be deleted is same as the id os the logged in user then only delete the post.
+        if (post.user == req.user.id)
         {
             post.remove();
-            Comment.deleteMany({post:req.params.id}, function(err)
-            {
-                if(err)
-                {
-                    console.log('Unable to delete all the comments of the post.');
-                    return;
-                }
-                return res.redirect('back');
-            })
+
+            await Comment.deleteMany({ post: req.params.id });
+
+            return res.redirect('back');
         }
+        // else do not delete the post.
         else
         {
             return res.redirect('back');
         }
-    })
+    }
+    catch(error)
+    {
+        console.log('Error in destroying a post!', error);
+        return;
+    }
+
+
+
+    
 }
