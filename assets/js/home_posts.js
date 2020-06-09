6de4ebb89bf824928b2455f1adf784ca19b13ee2
parikshit singh <pk223933@gmail.com>
2020-06-09 23:48:05 +0530

    Installed jquery type definitions for intellisense

---
+++ assets/js/home_posts.js
@@ -0,0 +1,10 @@
+let create_post=()=>
+{
+    let new_post_form=$('#new-post-form');
+    new_post_form.submit((event)=>
+    {
+        event.preventDefault();
+        $.
+    });
+}
+create_post();
