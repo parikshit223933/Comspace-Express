d979e3b8d176048ad6b0466f647b8fca2522038b
parikshit singh <pk223933@gmail.com>
2020-06-09 17:57:14 +0530

    async awaited comments controller

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,56 +1,52 @@
-const Comments=require('../models/comment');
-const Post=require('../models/post');
+const Comments = require('../models/comment');
+const Post = require('../models/post');
 
-module.exports.create=function(req, res)
+module.exports.create = async function (req, res)
 {
-    Post.findById(req.body.post, function(error, post)
+    try
     {
-        if(error)
+        let post = await Post.findById(req.body.post);
+
+        if (post)
         {
-            console.log('unable to find the post on which the coment is done!');
-            return;
-        }
-        if(post)
-        {
-            Comments.create(
+            let comment = await Comments.create(
                 {
-                    content:req.body.content,
-                    post:req.body.post,
-                    user:req.user._id
-                },
-                function(error, comment)
-                {
-                    if(error)
-                    {
-                        console.log('Error while creating a new comment!');
-                        return;
-                    }
-                    // updating
-                    post.comments.push(comment);
-                    post.save();
-                    res.redirect('back')
-                }
-            )
+                    content: req.body.content,
+                    post: req.body.post,
+                    user: req.user._id
+                });
+            // updating
+            post.comments.push(comment);
+            post.save();
+            res.redirect('back')
         }
-    })
+    }
+    catch (error)
+    {
+        console.log('There was an error in creating a comment!', error);
+        return;
+    }
+
+
+
 }
 
-module.exports.destroy=function(req, res)
+module.exports.destroy = function (req, res)
 {
-    Comments.findById(req.params.id, function(error, comment)
+    Comments.findById(req.params.id, function (error, comment)
     {
-        if(error)
+        if (error)
         {
             console.log('there was an error in finding the comment to delete.');
             return;
         }
-        if(comment.user==req.user.id)
+        if (comment.user == req.user.id)
         {
-            let post_id=comment.post;
+            let post_id = comment.post;
             comment.remove();
-            Post.findByIdAndUpdate(post_id, {$pull:{comments:req.params.id}}, function(error, post)
+            Post.findByIdAndUpdate(post_id, { $pull: { comments: req.params.id } }, function (error, post)
             {
-                if(error)
+                if (error)
                 {
                     console.log('Error in updating the post while trying to delete the comment!');
                     return;
