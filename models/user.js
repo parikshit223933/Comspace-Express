f60ba8a29b8c753ed02eeb82233596aaeff38bf5
parikshit singh <pk223933@gmail.com>
2020-06-12 21:30:03 +0530

    configuring multer for user avatar

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose=require('mongoose');
+const multer=require('multer');
+const path=require('path');
+const AVATAR_PATH=path.join('/uploads/users/avatars');
+
 const user_schema=new mongoose.Schema({
     email:{
         type:String,
@@ -12,10 +16,27 @@ const user_schema=new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar:{
+        type:String,
     }
 }, {
     timestamps:true,
 });
+
+let storage=multer.diskStorage(
+    {
+        destination:function(req, file, cb)
+        {
+            cb(null, path.join(__dirname, '..', AVATAR_PATH));
+        },
+        filename(req, file, callback)
+        {
+            cb(null, file.fieldname+'-'+Date.now());
+        }
+    }
+)
+
 
 const user=mongoose.model('User', user_schema);
 
