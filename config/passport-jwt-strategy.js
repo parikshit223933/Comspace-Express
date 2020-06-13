e82a2bc3de679c6cf5056d98de503e769da83a03
parikshit singh <pk223933@gmail.com>
2020-06-14 01:46:01 +0530

    created passport-jwt-strategy.js

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const jwt_strategy = require('passport-jwt').Strategy;
+const extract_jwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+
+let options =
+{
+    jwtFromRequest: extract_jwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'Comspace_Express',
+}
+passport.use(new jwt_strategy(options, function (data_from_jwt_payload, done)
+{
+    /* done is just a callback function, you can name it whatever you want */
+    User.findById(data_from_jwt_payload._id, function(error, user)
+    {
+        if(error)
+        {
+            console.log('Error in finding user from JWT!');
+            return;
+        }
+        if(user)
+        {
+            return done(null, user);
+        }
+        else
+        {
+            return done (null, false);
+        }
+    });
+}));
+module.exports=passport;
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
+const passportjwt=require('./config/passport-jwt-strategy');
 const flash=require('connect-flash');
 const mongoStore=require('connect-mongo')(session);
 const sassMiddleware=require('node-sass-middleware');
