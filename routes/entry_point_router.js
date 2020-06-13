c961e5dc715128855ad237f89e1e8a9b495d6df1
parikshit singh <pk223933@gmail.com>
2020-06-13 18:29:51 +0530

    set up folder structure for api

--- routes/entry_point_router.js
+++ routes/entry_point_router.js
@@ -4,10 +4,13 @@ const users_router=require('./users');
 const home_controller=require('../controllers/home_controller');
 const posts_router=require('./posts');
 const comments_router=require('./comments');
+const api=require('./api')
 
 router.get('/', home_controller.home);
 router.use('/users', users_router);
 router.use('/posts', posts_router);
 router.use('/comments', comments_router);
+
+router.use('/api', api);
 
 module.exports=router;
