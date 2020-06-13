400fa8261c0e0bc316dd3d46c50b158275bcb561
parikshit singh <pk223933@gmail.com>
2020-06-14 00:38:00 +0530

    shown all the database posts in the api v1

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,10 +1,26 @@
-module.exports.index=function(req, res)
+const Post=require('../../../models/post');
+
+module.exports.index= async function(req, res)
 {
+    let posts= await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate(
+        {
+            path:'comments',
+            populate:
+            {
+                path:'user'
+            }
+        }
+    );
+
+
     return res.status(200).json(
         {
             message:'List of Posts',
             api_version:'v1',
-            posts:[]
+            posts:posts
         }
     )
 }
