963cfe94229ecdca32e7a6fe5b8b371718300d44
parikshit singh <pk223933@gmail.com>
2020-06-15 02:51:53 +0530

    installed nodemailer

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/connect-flash/index.d.ts" />
 /// <reference path="globals/express-session/index.d.ts" />
 /// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/nodemailer/index.d.ts" />
 /// <reference path="globals/passport/index.d.ts" />
