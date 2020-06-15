ecbf7282e686685a07dcaf22a250d30025fbd351
parikshit singh <pk223933@gmail.com>
2020-06-15 19:10:03 +0530

    mailing template for new post created.

--- mailers/posts_mailer.js
+++ mailers/posts_mailer.js
@@ -2,13 +2,13 @@ const nodemailer_config=require('../config/nodemailer');
 
 module.exports.new_post=(post)=>
 {
-    console.log('Inside new post mailer!', post);
+    let HTMLstring=nodemailer_config.renderTemplate({post:post}, '/posts/new_post.ejs')
     nodemailer_config.transporter.sendMail(
         {
             from:'pk223933@gmail.com',
             to:post.user.email,
             subject:'Comspace Express Post published',
-            html:`<h1>Congratulations, You new post is live on the website</h1>`,
+            html:HTMLstring,
         },
         (error, info)=>
         {
