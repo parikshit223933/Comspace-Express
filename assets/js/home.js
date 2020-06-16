b7660f426e26c76f0b572c96706972a167736dcd
parikshit singh <pk223933@gmail.com>
2020-06-17 03:54:52 +0530

    basic toggle like button implemented

---
+++ assets/js/home.js
@@ -0,0 +1,29 @@
+let add_click_action_to_like_button= function()
+{
+    for(let like_button of $('.like-buttons'))
+    {
+        let button=$(like_button);
+        button.click(function()
+        {
+            if(button[0].dataset.toggle=='false')
+            {
+                button.find($('svg')).toggleClass(['far', 'fas']);
+                button.attr('data-toggle', 'true');
+                button.addClass('text-danger');
+
+                let likes=button.find($('span'));
+                likes.text(parseInt(likes.text())+1);
+            }
+            else
+            {
+                button.find($('svg')).toggleClass(['fas', 'far']);
+                button.attr('data-toggle', 'false');
+                button.removeClass('text-danger');
+
+                let likes=button.find($('span'));
+                likes.text(parseInt(likes.text())-1);
+            }
+        })
+    }
+}
+add_click_action_to_like_button()
