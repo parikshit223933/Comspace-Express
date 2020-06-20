846fd9063c7fbbd63d1c0dcd94f36bf753e4a72b
parikshit singh <pk223933@gmail.com>
2020-06-20 09:44:25 +0530

    shown the friends list on the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-
+const Friendship = require('../models/friendship');
 
 module.exports.home = async function (req, res)
 {
@@ -19,7 +19,7 @@ module.exports.home = async function (req, res)
                     },
                     populate:
                     {
-                        path:'likes'
+                        path: 'likes'
                     }
                 }
             )
@@ -28,12 +28,41 @@ module.exports.home = async function (req, res)
         //step 2= finding all the users which will then be passed to the rendering function.
         let users = await User.find({});
 
+        /* new step 4: finding the friends of the logged in user */
+        let friends = new Array();
+        if (req.user)/* friends list will only be loaded if thhe user is signed in */
+        {
+            let all_friendships = await Friendship.find({ $or: [{ from_user: req.user._id }, { to_user: req.user._id }] })
+                .populate('from_user')
+                .populate('to_user');/* checking the friendship model in the fields "from user" and "to_user". the current logged in user has to be in one of them. and at the same time we are also populating it to see the user ids*/
+
+            for (let fs of all_friendships)/* storing all the friendships in an array so that it is easy to load them in the front end quickly */
+            {
+                if (fs.from_user._id.toString() == req.user._id.toString())
+                {
+                    friends.push({
+                        friend_name: fs.to_user.name,
+                        friend_id: fs.to_user._id
+                    });
+                }
+                else if (fs.to_user._id.toString() == req.user._id.toString())
+                {
+                    friends.push({
+                        friend_name: fs.from_user.name,
+                        friend_id: fs.from_user._id
+                    });
+                }
+            }
+        }
+
+
         //step 3= rendering the page with all the posts and passing all the users to it.
         var options =
         {
             title: "ComSpace Express",
             posts: posts,
-            all_users: users
+            all_users: users,
+            friends: friends
         };
         return res.render('home', options);
     }
