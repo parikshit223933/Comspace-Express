b9764d41eb519f154455dbac886ee96b1aca98b0
parikshit singh <pk223933@gmail.com>
2020-06-22 07:39:09 +0530

    js and image minification using gulp completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,77 @@
-const gulp=require('gulp');
-const sass=require('gulp-sass');
-const cssnano=require('gulp-cssnano');
-const rev=require('gulp-rev');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const path=require('path');
 
-gulp.task('css',  function(done)
+gulp.task('css', function (done)
 {
     console.log('Minifying CSS...');
 
-     gulp.src('./assets/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css'))
-
-    .pipe(gulp.src('./assets/**/*.css'))
-     
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets/css'))
-    .pipe(rev.manifest({
-        cwd:'public',
-        merge:true
-    }))
-    .pipe(gulp.dest('./public/assets/css'));
+    gulp.src('./assets/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('./assets/css'))
+    gulp.src('./assets/**/*.css')
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
     done()
+});
+
+
+gulp.task('js', function (done)
+{
+    console.log('minifying js...');
+
+    gulp.src('./assets/**/*.js')
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
+
+    done();
+})
+
+gulp.task('images', function (done)
+{
+    console.log('Compressing Images');
+
+    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+        .pipe(imagemin())
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
+
+    done();
+});
+
+
+//this task will clear the public/assets directory
+gulp.task('clean:assets', function (done)
+{
+    del.sync('./public/assets/**');
+    done();
+});
+
+// main builder
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function (done)
+{
+    console.log('Building Assets');
+    done();
 })
